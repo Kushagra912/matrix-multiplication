4b88a3bb28c4b89c9bf09f738a7df3b4d8cd376e
Nalla Prakash <hamhaunalla99@gmail>
2021-12-17 12:03:41 +0530

    reducing lines for some reason

--- matrix.py
+++ matrix.py
@@ -1,6 +1,5 @@
 from operator import mul
-X=[[1,2,3], [4,5,6], [7,8,9]]
-Y=[[10,11,12], [13,14,15], [16,17,18]]
+X, Y=[[1,2,3], [4,5,6], [7,8,9]], [[10,11,12], [13,14,15], [16,17,18]]
 R = [[sum(mul(*es) for es in zip(r, c)) for c in zip(*Y)] for r in X]
 print("======Multiplied matrix====== \n")
 for r in R: print(r)
