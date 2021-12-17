3300e8e389f22b1e388d864b1256504653315288
Nalla Prakash <hamhaunalla99@gmail>
2021-12-17 12:00:06 +0530

    multiplying matrices in a more pythonic way

--- matrix.py
+++ matrix.py
@@ -1,16 +1,6 @@
-X=[[1,2,3],
-     [4,5,6],
-     [7,8,9]]
-Y=[[10,11,12],
-     [13,14,15],
-     [16,17,18]]
-R=[[0,0,0],
-   [0,0,0],
-   [0,0,0]]
-for i in range(len(X)):
-    for j in range(len(Y[0])):
-        for k in range(len(Y)):
-            R[i][j]+=X[i][k]*Y[k][j]
+from operator import mul
+X=[[1,2,3], [4,5,6], [7,8,9]]
+Y=[[10,11,12], [13,14,15], [16,17,18]]
+R = [[sum(mul(*es) for es in zip(r, c)) for c in zip(*Y)] for r in X]
 print("======Multiplied matrix====== \n")
-for r in R:
-    print(r)
+for r in R: print(r)
